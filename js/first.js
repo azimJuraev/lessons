aa9ced0c654fd08d83e709ccd3a8b90d29c65b76
Azim <example@gmail.com>
2020-06-22 23:47:49 +0400

    First commit

---
+++ js/first.js
@@ -0,0 +1,18 @@
+
+const name = 'Azimjon',
+      age = 25;
+      alert(name);
+console.log(`My name's ${name} I am ${age} years old`);
+
+let bool = true;
+console.log(bool);
+
+let num = 500;
+const leftBorderWidth = '5px';
+
+console.log(`Ширына контента ${num} граница ${leftBorderWidth}`);
+
+let number = 50;
+console.log(number);
+
+
---
+++ js/homepage.js
@@ -0,0 +1,5 @@
+'use strict'
+
+let str = 'fgfggg';
+
+console.log(str[0]);
